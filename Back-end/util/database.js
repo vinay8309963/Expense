98eb8c28c9165e4a6acaf51271c3390d0d3058b6
Vinay <vinay8309963@gmail.com>
2023-12-19 12:43:51 +0530

    Updated

--- Back-end/util/database.js
+++ Back-end/util/database.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv')
+dotenv.config()
 
-const sequelize = new Sequelize('expenses' ,'root' , 'vinay@992106',{
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
     host: 'localhost'
 });
